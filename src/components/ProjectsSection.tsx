9d3d21c49e99d54b10224e7f0770ebed74abed36
Yaashi2004 <yaashiikaa9876@gmail.com>
2026-05-28 21:40:30 +0530

    updated

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -24,7 +24,7 @@ const ProjectsSection = () => {
     "ESP32",
     "Arduino IDE",
   ],
-      image: "/Screenshot (438).png",
+      image: "/cold.jpeg",
       github: "https://github.com/yaashi2004/iot-room-occupancy-tracker",
       live: "https://github.com/yaashi2004/iot-room-occupancy-tracker"
     },
@@ -37,7 +37,7 @@ const ProjectsSection = () => {
     "LangChain",
     "Groq API",
     "Gmail API",],
-      image: "/cold.jpeg",
+      image: "/Screenshot (438).png",
       github: "https://github.com/yaashi2004/Cold-email-Generator",
       live: "https://cold-email-generator-w8co.onrender.com/"
     },
